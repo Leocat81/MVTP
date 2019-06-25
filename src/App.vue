64dd8587fd9c266daf72761e70244435441adddd
pipe <1173882893@qq.com>
2019-06-25 23:45:12 +0800

    循环图片和展示图片

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div id="app">
+    <div class="demo-image">
+      <div class="block" v-for="fit in fits" :key="fit">
+        <!--<span class="demonstration">{{ fit }}</span>-->
+        <el-image
+                style="width: 100px; height: 100px;float: left;"
+                :src="url"
+                :fit="fit"></el-image>
+      </div>
+    </div>
     <el-input v-model="input" placeholder="请输入内容"></el-input>
     <div>
       <el-button @click="startHacking">Start</el-button>
@@ -31,7 +40,9 @@ export default {
   },
   data() {
     return {
-      input: ''
+      input: '',
+      fits: ['fill', 'contain', 'cover', 'none', 'scale-down'],
+      url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
     }
   }
 
@@ -44,4 +55,7 @@ export default {
   font-family: Helvetica, sans-serif;
   text-align: center;
 }
+  #{
+  
+  }
 </style>
